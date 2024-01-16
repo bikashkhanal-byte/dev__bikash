92e67c8c83aac7140ab66d25715e027c523ddcb2
bikashkhanal-byte <bikashkhanak760@gmail.com>
2024-01-16 22:01:58 +0545

    Insta Web PAge

---
+++ C/C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[] = "winter";
+    int date = 25;
+    printf("This is a %s\n", name);
+    printf("%s is very cold\n", name);
+    printf("we need to wear warm clothes\n");
+    printf("It occur in %d december\n", date);
+    return 0;
+}
+
